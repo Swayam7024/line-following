785284af69112d3c83ee6f62f173a4653a0a1fb9
swayam khantwal <swayamkhantwal225@gmail.com>
2025-08-01 11:46:56 +0530

    initial code edited

--- line_following_robot.ino
+++ line_following_robot.ino
@@ -1,89 +1,114 @@
-// Line Sensor Pins
+// Motor Pins
+#define ENA 9
+#define ENB 10
+#define IN1 7
+#define IN2 8
+#define IN3 5
+#define IN4 6
+
+// Sensor Pins
 #define S1 A0
 #define S2 A1
 #define S3 A2
 #define S4 A3
 #define S5 A4
 
-// Motor Driver Pins
-#define ENA 9
-#define IN1 8
-#define IN2 7
-
-#define ENB 10
-#define IN3 6
-#define IN4 5
-
-int baseSpeed = 150;    // Adjust this for your motor torque
-float Kp = 30;           // Proportional constant
-float Kd = 20;           // Derivative constant
-int lastError = 0;
+// Threshold for black/white detection
+int threshold = 500; // You might need to calibrate this
 
 void setup() {
-  // Sensor Inputs
+  // Motor setup
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  // Sensor setup
   pinMode(S1, INPUT);
   pinMode(S2, INPUT);
   pinMode(S3, INPUT);
   pinMode(S4, INPUT);
   pinMode(S5, INPUT);
 
-  // Motor Outputs
-  pinMode(ENA, OUTPUT);
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-
-  pinMode(ENB, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // Read sensors as analog
-  int s[5];
-  s[0] = analogRead(S1);
-  s[1] = analogRead(S2);
-  s[2] = analogRead(S3);
-  s[3] = analogRead(S4);
-  s[4] = analogRead(S5);
+  // Read sensor values
+  int s1 = analogRead(S1);
+  int s2 = analogRead(S2);
+  int s3 = analogRead(S3);
+  int s4 = analogRead(S4);
+  int s5 = analogRead(S5);
 
-  // Threshold the sensors (black = 0, white = 1)
-  for (int i = 0; i < 5; i++) {
-    if (s[i] > 500) s[i] = 1;  // white surface
-    else s[i] = 0;             // black line
+  // Convert to digital (black = LOW, white = HIGH)
+  bool s1Black = s1 < threshold;
+  bool s2Black = s2 < threshold;
+  bool s3Black = s3 < threshold;
+  bool s4Black = s4 < threshold;
+  bool s5Black = s5 < threshold;
+
+  // Debug
+  Serial.print(s1Black); Serial.print(" ");
+  Serial.print(s2Black); Serial.print(" ");
+  Serial.print(s3Black); Serial.print(" ");
+  Serial.print(s4Black); Serial.print(" ");
+  Serial.println(s5Black);
+
+  // Movement logic
+  if (s3Black && !s1Black && !s2Black && !s4Black && !s5Black) {
+    // Centered - go straight
+    moveForward();
+  }
+  else if (s2Black || s1Black) {
+    // Turn left
+    turnLeft();
+  }
+  else if (s4Black || s5Black) {
+    // Turn right
+    turnRight();
+  }
+  else {
+    // Lost - stop or search
+    stopMotors();
   }
 
-  // Calculate error based on position
-  int error = 0;
-
-  if (s[0] == 0) error = -4;
-  else if (s[1] == 0) error = -2;
-  else if (s[2] == 0) error = 0;
-  else if (s[3] == 0) error = 2;
-  else if (s[4] == 0) error = 4;
-  else error = lastError;  // if all white, use last known error
-
-  int correction = Kp * error + Kd * (error - lastError);
-  lastError = error;
-
-  // Compute motor speeds
-  int leftSpeed = baseSpeed - correction;
-  int rightSpeed = baseSpeed + correction;
-
-  leftSpeed = constrain(leftSpeed, 0, 255);
-  rightSpeed = constrain(rightSpeed, 0, 255);
-
-  // Move motors with PWM
-  moveMotors(leftSpeed, rightSpeed);
+  delay(10);
 }
 
-void moveMotors(int leftPWM, int rightPWM) {
-  // Left motor forward
+// Movement functions
+void moveForward() {
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
-  analogWrite(ENA, leftPWM);
-
-  // Right motor forward
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  analogWrite(ENB, rightPWM);
+  analogWrite(ENA, 150);
+  analogWrite(ENB, 150);
+}
+
+void turnLeft() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH); // Left motor backward
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);  // Right motor forward
+  analogWrite(ENA, 120);
+  analogWrite(ENB, 150);
+}
+
+void turnRight() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);  // Left motor forward
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH); // Right motor backward
+  analogWrite(ENA, 150);
+  analogWrite(ENB, 120);
+}
+
+void stopMotors() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
 }
